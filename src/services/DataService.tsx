45ef8374b40ea5007ddef9a8bdb8635c36e65368
sivaganesh193 <67482763+sivaganesh193@users.noreply.github.com>
2023-11-25 20:02:20 +0530

    LOGIN Page

---
+++ src/services/DataService.tsx
@@ -0,0 +1,31 @@
+import { environment } from "../environment";
+
+const BASE_URL = environment.domainUrl;
+
+const dataService = {
+    signup: async (userData: User) => {
+        try {
+            const response = await fetch(`/db/signup`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+                    'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+                },
+                body: JSON.stringify(userData),
+            });
+
+            if (!response.ok) {
+                throw new Error('Signup failed');
+            }
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error during signup:', error);
+            throw error;
+        }
+    },
+};
+
+export default dataService;
