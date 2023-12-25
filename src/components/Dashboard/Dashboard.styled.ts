9168865f8a6a3ef548294128714d84277fc78b75
sivaganesh193 <67482763+sivaganesh193@users.noreply.github.com>
2023-12-25 07:42:17 +0530

    UI changes for dashboard

--- src/components/Dashboard/Dashboard.styled.ts
+++ src/components/Dashboard/Dashboard.styled.ts
@@ -3,19 +3,36 @@ import styled from 'styled-components';
 export const DashboardWrapper = styled.div`
 display:flex;
 height:-webkit-fill-available;
+padding-top: 70px; 
 `;
 
-export const ContentArea = styled.div`
+export const ContentArea = styled.div<{isSmallScreen: boolean}>`
 background-color: whitesmoke;
-width:80%;
+width:100%;
 margin-right:0;
 color: black;
+overflow-y: auto;
 `;
-
 
 export const HeaderWrapper = styled.div`
 background-color:white;
 padding:10px;
 color: #370342;
-box-shadow: 10px 10px #370342;
+display: flex;
+position: fixed;
+top: 0;
+left: 0;
+right: 0;
+z-index: 1000; /* Adjust z-index as needed */
 `;
+
+export const BottomNavWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #fff; /* Add your background color */
+  z-index: 1000; /* Adjust z-index as needed */
+  /* Add other styling as needed */
+`;
+
