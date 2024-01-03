f814828f0f25ab0091c8307c079ca7a5fd466e97
sivaganesh193 <sivaganeshb193@gmail.com>
2024-01-03 14:58:08 +0530

    Remove import

--- src/components/Events/EventCard/EventCard.tsx
+++ src/components/Events/EventCard/EventCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { EventCardWrapper } from './EventCard.styled';
 import { Card, CardContent, Typography } from '@mui/material';
 import { toTitleCase } from '../../../services/State';
