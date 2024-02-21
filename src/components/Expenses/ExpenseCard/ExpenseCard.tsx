a613ead4483f264dcaee561719dfce23a609ae16
adi-codes <71934760+adi-codes@users.noreply.github.com>
2024-02-21 19:26:07 +0530

    expense card changes

--- src/components/Expenses/ExpenseCard/ExpenseCard.tsx
+++ src/components/Expenses/ExpenseCard/ExpenseCard.tsx
@@ -9,23 +9,45 @@ interface ExpenseCardProps {
 
 const ExpenseCard: FC<ExpenseCardProps> = ({expense}) => {
 
+   const paid = expense.paidBy==localStorage.getItem('userId')
+   let owe=0
+   let owed=0
+   if (paid){
+      expense.shares.map((share)=>{
+         if(share.userId!==localStorage.getItem('userId'))
+            owed+=share.amount
+      })
+   } else{
+      expense.shares.map((share)=>{
+         if(share.userId==localStorage.getItem('userId'))
+            owe+=share.amount
+      })
+   }
    return (
         <ExpenseCardWrapper>
           <Card>
             <CardContent>
                <Row>
-                  <Col sm={6} className="d-flex justify-content-center align-items-center">
+                  <Col sm={2} className="d-flex justify-content-center align-items-center">
                      <div className="text-left"> {/* Add text-left class for left alignment inside the div */}
                         <Typography variant="h5" component="div">
                            {toTitleCase(expense.expenseName)}
                         </Typography>
                      </div>
                   </Col>
-
+                  <Col sm={5} className="d-flex justify-content-right align-items-right">
+                     <div className="text-right"> {/* Add text-left class for left alignment inside the div */}
+                     { !paid? 
+                        
+                        <div>
+                           You owe {owe}
+                        </div>:<div>  You are owed {owed}</div>
+                     }
+                     </div>
+                  </Col>
                </Row>
             </CardContent>
          </Card>
-         {expense.expenseName}
         </ExpenseCardWrapper>
    );
 };
