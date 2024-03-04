3fc531a28eb9b3e598c660b4c0847f0d15d3b36e
Saroja <sarojasubramanian77@gmail.com>
2024-03-04 11:25:37 +0530

    adding icon in side nav

--- src/components/SideNavBar/SideNavBar.tsx
+++ src/components/SideNavBar/SideNavBar.tsx
@@ -1,6 +1,11 @@
 import { FC, useEffect, useState } from 'react';
 import { NavBar, SideNavBarWrapper } from './SideNavBar.styled';
 import './SideNavBar.css';
+import { MdDashboard } from "react-icons/md";
+import { TiGroup } from "react-icons/ti";
+import { RiAccountBoxFill } from "react-icons/ri";
+import { FaMoneyBill1Wave } from "react-icons/fa6";
+
 
 interface SideNavBarProps {
    onSelectContent: (content: string) => void;
@@ -36,24 +41,28 @@ const SideNavBar: FC<SideNavBarProps> = ({ onSelectContent }) => {
                   className={`nav-item ${activeItem === 'Events' ? 'active' : ''}`}
                   onClick={() => handleItemClick('Events')}
                >
+                  <MdDashboard style={{ fontSize: 'x-large', marginRight: '10px' }}></MdDashboard>
                   Events
                </div>
                <div
                   className={`nav-item ${activeItem === 'Friends' ? 'active' : ''}`}
                   onClick={() => handleItemClick('Friends')}
                >
+                  <TiGroup style={{ fontSize: 'x-large', marginRight: '10px' }}></TiGroup>
                   Friends
                </div>
                <div
                   className={`nav-item ${activeItem === 'Personal Expenses' ? 'active' : ''}`}
                   onClick={() => handleItemClick('Personal Expenses')}
                >
+                  <FaMoneyBill1Wave style={{ fontSize: 'x-large', marginRight: '10px' }}></FaMoneyBill1Wave>
                   Personal Expenses
                </div>
                <div
                   className={`nav-item ${activeItem === 'Account' ? 'active' : ''}`}
                   onClick={() => handleItemClick('Account')}
                >
+                  <RiAccountBoxFill style={{ fontSize: 'x-large', marginRight: '10px' }}></RiAccountBoxFill>
                   Account
                </div>
             </NavBar>
