adcd9bcfa3e831f2d027e3896fdd9f04748848ad
adi-codes <71934760+adi-codes@users.noreply.github.com>
2024-03-31 21:15:32 +0530

    adv settle upi

--- src/components/Expenses/SettleExpenseModal/SettleExpenseModal.tsx
+++ src/components/Expenses/SettleExpenseModal/SettleExpenseModal.tsx
@@ -1,29 +1,95 @@
-import { useState } from 'react';
-import { Box, Modal } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Box, Card, CardContent, Modal, TextField, styled } from '@mui/material';
 import { Flex, Container } from '../../../App.styled';
 import QRCode from 'react-qr-code';
 import VerifiedUserOutlinedIcon from '@mui/icons-material/VerifiedUserOutlined';
 import Tooltip from '@mui/material/Tooltip';
 import { IconButton, Typography } from '@mui/material';
 import QrCode2OutlinedIcon from '@mui/icons-material/QrCode2Outlined';
-
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CurrencyRupeeIcon from '@mui/icons-material/CurrencyRupee';
+import { toTitleCase } from '../../../services/State';
+import { Col, Row } from 'react-bootstrap';
+import { MarginLeft } from '../ExpenseCardNew/ExpenseCardNew.styled';
+import dataService from '../../../services/DataService';
 
 interface SettleExpenseModalProps {
     onClose: () => void;
-    submitSettleUp: () => void;
-    link: string
-    friendData: any
+    friendSettleUp?: () => void;
+    eventSettleUp?:(shares:{userId:string,amount:number}[]) => void;
+    friendData: {id:string;name:string;due:number}[]
+    settleType:string
 }
 
-const SettleExpenseModal: React.FC<SettleExpenseModalProps> = ({ onClose, submitSettleUp, link , friendData }) => {
+const SettleExpenseModal: React.FC<SettleExpenseModalProps> = ({ onClose, friendSettleUp,eventSettleUp, friendData ,settleType}) => {
+    const appTitle = import.meta.env.VITE_APP_TITLE;
     const [showQR, setshowQR] = useState(false);
-
+    const [selectedFriendData, setSelectedFriendData] = useState<{id:string;name:string;due:number}>({id:"",name:"",due:0})
+    const [amountConfirmed,setAmountConfirmed] =useState(false)
+    const  [link,setLink]=useState("")
+    const CenteredFlex = styled(Flex)({
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+       marginBottom: '-5px',
+      });
+    const HorizontalCenteredFlex = styled(Flex)({
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    });
+      
+    const StyledCurrencyRupeeIcon = styled(CurrencyRupeeIcon)({
+        marginRight: '4px', // Adjust the margin as needed
+      });
     const verifySubmit = async() => {
-        const response = confirm("Please confirm that you have paid Rs " + friendData.due + " to " + friendData.name);
+        const response = confirm("Please confirm that you have paid Rs " + selectedFriendData.due + " to " + selectedFriendData.name);
         if (response) {
-            submitSettleUp();
+            if(settleType=="friend"){
+                friendSettleUp!();
+            }else{
+                const shares=[{userId:selectedFriendData.id,amount:selectedFriendData.due}]
+                console.log("shares",shares)
+                eventSettleUp!(shares);
+            }
+                
         }
     }
+    const handleConfirmAmount = async() =>{
+        setAmountConfirmed(true)
+        try{
+            const result = await dataService.generateUPILink(selectedFriendData.due, 'Settle ' + appTitle + ' dues', selectedFriendData.id);
+            if (result.message === "No record found for id") {
+                const response = confirm("Receiving has not added UPI ID to their account.\nDo you want to proceed to settle manually ? ");
+                if (response) {
+                    if(settleType=="friend"){
+                        friendSettleUp!();
+                    }else{
+                        const shares=[{userId:selectedFriendData.id,amount:selectedFriendData.due}]
+                        eventSettleUp!(shares);
+                    }    
+                }else{
+                    onClose()
+                }
+            } else {
+                setLink(result.upiLink);
+            }
+        } catch (error) {
+            console.error("Error while generating UPI Link", error);
+        }
+
+    }
+
+    const handleSelectSettleUser =  (data:{id:string;name:string;due:number})=>{
+        setSelectedFriendData(data);
+
+    }
+
+    useEffect(()=>{
+        if(friendData.length==1){
+            setSelectedFriendData(friendData[0]);
+        }
+    },[])
 
     return (
         <Container>
@@ -34,6 +100,77 @@ const SettleExpenseModal: React.FC<SettleExpenseModalProps> = ({ onClose, submit
                 aria-describedby="change-pwd-modal-description"
             >
                 <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', minWidth: 350, bgcolor: 'background.paper', boxShadow: 24, p: 4, borderRadius: 1 }}>
+                   
+                    {selectedFriendData.id=="" &&
+                        (<CenteredFlex> 
+                        <Typography variant="h6" component="div" >
+                                                Who do you want to settle with:
+                        </Typography>
+                        {friendData.map((data)=>{
+                            return (
+                            
+                                <Card sx={{"width":"100%"}} onClick={()=>{handleSelectSettleUser(data)
+                                }}>
+                            
+                                    <CardContent>
+                                        
+                                        <Row>
+                                            <Col className="d-flex ">
+                                                <MarginLeft className="text-left"> {/* Add text-left class for left alignment inside the div */}
+                                                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold', textTransform: 'capitalize' }}>
+                                                        {toTitleCase(data.name)}
+                                                    </Typography>
+                                                </MarginLeft>
+                                            </Col>
+                                            <br></br>
+                                            <Col className="d-flex justify-content-end">
+                                                <HorizontalCenteredFlex sx={{'gap':'4px'}}>
+                                                    <StyledCurrencyRupeeIcon fontSize="small"/>
+                                                    <Typography variant="h6" component="div" >
+                                                                {data.due}
+                                                    </Typography>
+                                                </HorizontalCenteredFlex>
+                                            </Col>
+                                        </Row>
+                                    </CardContent>
+                                    <Typography variant="h4"><b>{selectedFriendData.name}</b> </Typography>
+                                </Card>
+                            )
+                        })}
+                        </CenteredFlex>)
+                    
+                    }
+                    {!amountConfirmed && selectedFriendData.id!="" && 
+                         <CenteredFlex>
+                         <Typography variant="h4"><b>You </b><ArrowForwardIcon/> <b> {selectedFriendData.name}</b></Typography>
+    
+                         <Typography variant="h5">You are going to pay:</Typography>
+                         <HorizontalCenteredFlex>
+                            <CurrencyRupeeIcon/>
+                            <TextField
+                           type="number"
+                           onChange={(event) => {
+                            const updatedFriendData ={
+                                ...selectedFriendData,
+                                due : parseFloat(event.target.value)
+                            }
+                            console.log("inchange",selectedFriendData)
+                            setSelectedFriendData(updatedFriendData)
+                           }}
+                           value={selectedFriendData.due}
+                           name="amount"
+                           required
+                            />
+                            
+                         </HorizontalCenteredFlex>
+                         <br/>
+                         <button className="w-100" style={{marginBottom: 0}} onClick={handleConfirmAmount}>
+                                <span>Confirm</span>
+                            </button>
+                       </CenteredFlex>
+                    }{
+                        amountConfirmed && link=="" && <Typography variant="h4">Awaiting confirmation...</Typography>
+                    }{amountConfirmed && link!="" && <div>
                     <Flex style={{marginBottom: '-5px'}}>
                         <Typography variant="h4">UPI Payment Helper</Typography>
                         <Tooltip title={showQR ? "Hide QR" : "View QR"}>
@@ -90,6 +227,7 @@ const SettleExpenseModal: React.FC<SettleExpenseModalProps> = ({ onClose, submit
                     <button className="w-100" style={{marginBottom: 0}} onClick={verifySubmit}>
                         <span>Settle Up!</span>
                     </button>
+                </div>}
                 </Box>
             </Modal>
         </Container >
