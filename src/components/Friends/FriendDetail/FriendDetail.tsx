0bd6e97f66c1c016cba0b4b1b642cc51e2123f9f
Saroja <sarojasubramanian77@gmail.com>
2024-03-31 11:37:05 +0530

    pagination

--- src/components/Friends/FriendDetail/FriendDetail.tsx
+++ src/components/Friends/FriendDetail/FriendDetail.tsx
@@ -4,7 +4,7 @@ import dataService from '../../../services/DataService';
 import { FriendDetailWrapper } from './FriendDetail.styled';
 import { IoMdArrowBack } from "react-icons/io";
 import { IoPersonRemoveOutline } from "react-icons/io5";
-import { Typography } from '@mui/material';
+import { CircularProgress, List, Pagination, Typography } from '@mui/material';
 import ExpenseCardNew from '../../Expenses/ExpenseCardNew/ExpenseCardNew';
 import { Row, Col } from 'react-bootstrap';
 import { MdOutlinePlaylistAdd } from 'react-icons/md';
@@ -17,70 +17,78 @@ function FriendDetail() {
         "name": "",
         "overallOweAmount": 0,
         "overallWhoOwes": "",
-        "expenses": []
+        "expenses": [{
+            "expenseId":""
+        }]
     });
-    const [showData, setShowData] = useState(false);
-    const [link, setlink] = useState('');
-    const [isModalOpen, setisModalOpen] = useState(false);
+    const [link, setLink] = useState('');
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const [refetchData, setRefetchData] = useState(false);
+    const [showLoader, setShowLoader] = useState(true);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 5;
+    const navigate = useNavigate();
     const { friendId } = useParams<{ friendId: string }>();
 
-    const navigate = useNavigate();
     const handleGoBack = () => {
         navigate(-1);
     };
+
+    const handleChangePage = (_event: React.ChangeEvent<unknown>, page: number) => {
+        setCurrentPage(page);
+    };
+
     const handleSettleUpFriend = async () => {
-        if (friendId != undefined) {
+        if (friendId !== undefined) {
             try {
                 const result = await dataService.generateUPILink(friendData.overallOweAmount, 'Settle ' + appTitle + ' dues', friendId);
                 if (result.message === "No record found for id") {
-                    const response = confirm("Receiving has not added UPI ID to their account.\nDo you want to proceed to settle manually ? ");
+                    const response = window.confirm("Receiving has not added UPI ID to their account.\nDo you want to proceed to settle manually?");
                     if (response) {
                         await dataService.settleUpWithFriend(friendId).then((data) => {
-                            alert(data.message)
-                        })
+                            alert(data.message);
+                        });
                     }
-                }
-                else {
-                    setlink(result.upiLink);
-                    setisModalOpen(true);
+                } else {
+                    setLink(result.upiLink);
+                    setIsModalOpen(true);
                 }
             } catch (error) {
                 console.error("Error while generating UPI Link", error);
             }
         }
-    }
+    };
 
     const closeModal = () => {
-        setisModalOpen(false);
-    }
+        setIsModalOpen(false);
+    };
 
     const submitSettleUp = async () => {
-        setisModalOpen(false);
-        if (friendId != undefined) {
+        setIsModalOpen(false);
+        if (friendId !== undefined) {
             try {
                 await dataService.settleUpWithFriend(friendId).then((data) => {
                     alert(data.message);
                     setRefetchData(!refetchData);
-                })
+                });
             } catch (error) {
                 const errMsg = "Error while settling dues. Please try again later";
                 console.error(errMsg, error);
                 alert(errMsg);
             }
         }
-    }
+    };
 
     useEffect(() => {
         if (friendId) {
             dataService.getFriendDetails(friendId)
                 .then((data) => {
                     setFriendData(data);
-                    setShowData(true);
+                    setShowLoader(false);
                 })
                 .catch(error => console.error('Error fetching friend data:', error));
         }
-    }, [refetchData]); // useEffect dependency
+    }, [refetchData, friendId]); // useEffect dependency
 
     const handleCreateExpense = () => {
         navigate(`/createExpense/friend/${friendId}`);
@@ -88,19 +96,24 @@ function FriendDetail() {
 
     const handleRemoveFriend = async () => {
         await dataService.deleteFriend(friendData.uuid).then((data) => {
-            alert(data.message)
-            if (data.success == true)
-                navigate(-1)
-        })
-    }
+            alert(data.message);
+            if (data.success === true)
+                navigate(-1);
+        });
+    };
 
     const isMobile = window.innerWidth <= 500;
+
+    // Calculate start and end indexes for the current page
+    const indexOfLastExpense = currentPage * itemsPerPage;
+    const indexOfFirstExpense = indexOfLastExpense - itemsPerPage;
+    const currentExpenses = friendData.expenses.slice(indexOfFirstExpense, indexOfLastExpense);
 
     return (
         <FriendDetailWrapper>
             {isModalOpen && <SettleExpenseModal onClose={closeModal} submitSettleUp={submitSettleUp} link={link} friendData={{
                 name: friendData.name,
-                due: friendData.overallOweAmount 
+                due: friendData.overallOweAmount
             }}
             />}
             <Row>
@@ -124,39 +137,44 @@ function FriendDetail() {
                     </button>
                 </Col>
             </Row>
-            <div>
-                <h2>{friendData.name}</h2>
-                <Row className="align-items-center">
-                    <Col xs={6} md={9} className="d-flex align-items-center">
-                        {showData && (<Typography variant="h6">
-                            Overall,
-                            {friendData.overallWhoOwes === 'user' ? (
-                                'you owe '
-                            ) : (
-                                ` ${friendData.name} owes you `
-                            )}
-                            Rs.{friendData.overallOweAmount}
-                        </Typography>)}
-                    </Col>
-                    {(friendData.overallWhoOwes === 'user' && friendData.overallOweAmount != 0) &&
-                        <Col xs={6} md={3} className="d-flex align-items-center">
-                            <Row className="align-items-center">
-                                <div> You have unsetlled dues...
-                                    <button className="w-100" onClick={handleSettleUpFriend}>
-                                        <span>Settle Up!</span>
-                                    </button>
-                                </div>
-                            </Row>
-                        </Col>}
-                </Row>
-                <br></br>
-                {friendData.expenses && friendData.expenses.reverse().map((expense) => {
-                    return (
-                        <ExpenseCardNew expense={expense} />
-                    )
-                })}
-
-            </div>
+            {showLoader && (<div className="d-flex justify-content-center align-items-center"><CircularProgress color="secondary" variant="indeterminate" /></div>)}
+            {!showLoader && (
+                <div>
+                    <h2>{friendData.name}</h2>
+                    <Row className="align-items-center">
+                        <Col xs={6} md={9} className="d-flex align-items-center">
+                            {(<Typography variant="h6">
+                                Overall,
+                                {friendData.overallWhoOwes === 'user' ? (
+                                    'you owe '
+                                ) : (
+                                    ` ${friendData.name} owes you `
+                                )}
+                                Rs.{friendData.overallOweAmount}
+                            </Typography>)}
+                        </Col>
+                        {(friendData.overallWhoOwes === 'user' && friendData.overallOweAmount !== 0) &&
+                            <Col xs={6} md={3} className="d-flex align-items-center">
+                                <Row className="align-items-center">
+                                    <div> You have unsetlled dues...
+                                        <button className="w-100" onClick={handleSettleUpFriend}>
+                                            <span>Settle Up!</span>
+                                        </button>
+                                    </div>
+                                </Row>
+                            </Col>}
+                    </Row>
+                    <br></br>
+                    <List>
+                        {currentExpenses.map((expense) => (
+                            <div key={expense.expenseId}>
+                                <ExpenseCardNew expense={expense} />
+                            </div>
+                        ))}
+                    </List>
+                    <Pagination count={Math.ceil(friendData.expenses.length / itemsPerPage)} page={currentPage} onChange={handleChangePage} />
+                </div>
+            )}
         </FriendDetailWrapper>
     );
 }
