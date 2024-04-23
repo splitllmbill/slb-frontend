49735afd899a3a7dc633385c93fee2e5ef46a964
Saroja <sarojasubramanian77@gmail.com>
2024-04-23 19:51:04 +0530

    build fix

--- src/components/Friends/FriendsPage.tsx
+++ src/components/Friends/FriendsPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Row , Col } from "react-bootstrap";
 import { CircularProgress, List } from "@mui/material";
 import { AiOutlineUsergroupAdd } from "react-icons/ai";
