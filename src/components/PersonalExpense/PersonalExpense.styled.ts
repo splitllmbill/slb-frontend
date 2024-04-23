1b73477f0c81d508ee33fdac21dd883a7f623a38
Saroja <sarojasubramanian77@gmail.com>
2024-04-23 21:37:41 +0530

    personal expense css

--- src/components/PersonalExpense/PersonalExpense.styled.ts
+++ src/components/PersonalExpense/PersonalExpense.styled.ts
@@ -16,23 +16,22 @@ export const Input = styled.input`
 
 export const InputSmall = styled.input`
   flex: 1;
-  padding: 8px;
-  margin: -2px 20px 10px 20px;
+  padding: 6px;
+  margin: 0px 20px 10px 20px;
   background: #fff;
   border: 1px solid black;
   color: black;
-  height: 25px
 `;
 
 export const DatePickerSmall = styled(DatePicker)`
-  width: 120px;
-  border: 1px solid #ced4da;
-  background: inherit;
-  border-radius: 4px;
-  padding: 15px;
-  font-size: 1rem;
-  line-height: 1.5;
-  color: #495057;
-  background-clip: padding-box;
-  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+width: 100px;
+border: 1px solid black;
+background: inherit;
+/* border-radius: 4px; */
+padding: 7px; 
+font-size: 1rem;
+line-height: 1.5;
+/* color: #495057; */
+background-clip: padding-box;
+transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 `;
