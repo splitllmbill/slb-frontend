57ea1a6ab720223d2c6bc3e233dea377df012243
sivaganesh193 <sivaganeshb193@gmail.com>
2024-04-30 22:48:30 +0530

    try 4

--- middleware.ts
+++ middleware.ts
@@ -3,12 +3,14 @@ import { rewrite } from '@vercel/edge';
 export default function redirectMiddleware(request: Request) {
   // Read the backend URL from environment variables
   const backendURL = process.env.VITE_BACKEND_SERVER;
-
+  console.log(backendURL);
+  console.log(request.url);
   // If backend URL is not defined or the request is not for the /api context, return original request
   if (!backendURL || !request.url.startsWith('/api')) {
     return request;
   }
-
+  console.log(new URL(backendURL + request.url));
+  
   // Rewrite the request to the backend URL
   return rewrite(new URL(backendURL + request.url));
 }
